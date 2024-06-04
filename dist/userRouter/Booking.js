a98632cf5091adc88e81840b608c3a3bbc275e11
vuduyviet1110 <2001040225@s.hanu.edu.vn>
2024-06-05 04:01:01 +0700

    fix script

--- dist/userRouter/Booking.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var express = require("express");
-// import api của express là router
-var router = express.Router();
-var ReservationUserController = require("../controllers/BookingUserController");
-// đường dẫn gốc
-router.post("/", ReservationUserController.create);
-module.exports = router;
--- dist/userRouter/homePage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var express = require("express");
-// import api của express là router
-var router = express.Router();
-var HomePageController = require("../controllers/HomePageController");
-// đường dẫn gốc
-router.get("/:id", HomePageController.showInfo);
-module.exports = router;
